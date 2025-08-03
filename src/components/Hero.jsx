ef8c6e19ddf45a5413e6c8e823fbd73e2f64c936
shovon-gt <shovon.biswas@gigatechltd.com>
2025-08-04 00:01:04 +0600

    Fixed mobile responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
-import players from "../assets/PLAYERS.jpg"
+import players from "../assets/PLAYERS.jpg";
 const Hero = () => {
   return (
     <section
-      className="flex px-24 pt-6 "
+      className="md:flex px-2 md:px-24 pt-6 pb-3 "
     >
       <div className="">
         <div>
-          <h1 className="text-7xl text-white font-bold leading-tight mb-4">
+          <h1 className="text-5xl md:text-7xl text-white font-bold leading-tight mb-4">
             Prescription<br />
             Treatments For<br />
             Your <br /> <span className="text-yellow-400">Health Goals</span>
@@ -18,8 +18,8 @@ const Hero = () => {
       </div>
 
       <div
-        className=""
-        style={{ marginBottom: "-50px" }}
+        className="px-2"
+        // style={{ marginBottom: "-50px" }}
       >
         <img src={players} alt="Fighters" className="h-[400px] object-cover" />
       </div>
